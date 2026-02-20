246631e5403462ec68a4cba60ac1a87745916805
Ahmed <a.baaadr@gmail.com>
2026-02-20 21:45:39 +0200

    Add datatype.py with various data type examples and comments

---
+++ datatype.py
@@ -0,0 +1,50 @@
+import random as rd
+import datetime as dt
+
+# String (str)
+name = 'Ahmed'
+
+# Integer (int)
+age = 30
+
+# Float (float)
+salary = 1000.5
+
+# Boolean (bool)
+ismale = True
+
+# Set
+fullname = {'Ahmed', 'Badr'}
+
+# Dictionary (dict)
+human = {
+  name: 'Ahmed',
+  age: 30
+}
+
+# Tuple (tuple)
+number = (7, 8, 4)
+
+# Range (range)
+number_range = range(10)
+
+# List (list)
+my_list = [22, 'Badr', 22.5, 'Ismail']
+
+# None (NoneType)
+unknown = None
+
+# print(type(my_list))
+
+
+# isinstance function 
+# print(isinstance(name, str)) # True
+
+# print(int(salary))
+# print(float(age))
+
+# print(rd.randrange(500, 1000000))
+
+# list []
+# tuple ()
+# set {}
--- oop.py
+++
@@ -1,9 +0,0 @@
-
-sentance = 'I,Love,Python'
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def greet(self):
-        return f"Hello, my name is {self.name} and I am {self.age} years old."
